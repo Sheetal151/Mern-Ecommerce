a85ed22a8359568476e288d1144e1c6a76177f4d
Sheetal <138355965+Sheetal151@users.noreply.github.com>
2026-06-26 16:59:00 +0530

    Fix   analytics.route.js

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -3,7 +3,7 @@ import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.controller.js";
 
 const router = express.Router();
-
+ 
 router.get("/", protectRoute, adminRoute, async (req, res) => {
 	try {
 		const analyticsData = await getAnalyticsData();
