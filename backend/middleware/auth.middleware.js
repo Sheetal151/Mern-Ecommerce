eec00a3cdee77c125f1340cdaf8fe6c9982296e9
Sheetal <138355965+Sheetal151@users.noreply.github.com>
2026-06-26 16:44:14 +0530

    Fix formatting in auth middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -18,8 +18,7 @@ export const protectRoute = async (req, res, next) => {
 			}
 
 			req.user = user;
-
-			next();
+            next();
 		} catch (error) {
 			if (error.name === "TokenExpiredError") {
 				return res.status(401).json({ message: "Unauthorized - Access token expired" });
